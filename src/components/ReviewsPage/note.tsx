1b9b7cd9ab61d2ef99e14dcd6605e94dbdaac4c3
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2023-07-25 20:33:56 +0200

    Add reviews note

---
+++ src/components/ReviewsPage/note.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+function Note() {
+    return (
+        <div className="bu_reviews__note">
+            <FormattedMessage id="reviews_note" />
+        </div>
+    )
+
+}
+
+export default Note;
