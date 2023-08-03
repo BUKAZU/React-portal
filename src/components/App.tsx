86fe301c91efaf2e480537863c2c9e3cd03cb07e
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2023-08-03 16:12:44 +0200

    Fix small screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Loading from './icons/loading.svg';
 
 import 'react-date-picker/dist/DatePicker.css';
@@ -15,11 +15,59 @@ import ErrorBoundary from './ErrorBoundary';
 import { useQuery } from '@apollo/client';
 import { AppContext } from './AppContext';
 import { FiltersType } from './SearchPage/filters/filter_types';
+import { set } from 'date-fns';
+import { el } from 'date-fns/locale';
 
 const getWidth = () =>
   window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
+
+  export const useContainerDimensions = targetRef => {
+    const [dimensions, setDimensions] = useState({});
+
+    // holds the timer for setTimeout and clearInterval
+    let movement_timer = null;
+
+    // the number of ms the window size must stay the same size before the
+    // dimension state variable is reset
+    const RESET_TIMEOUT = 100;
+
+    const test_dimensions = () => {
+      // For some reason targetRef.current.getBoundingClientRect was not available
+      // I found this worked for me, but unfortunately I can't find the
+      // documentation to explain this experience
+      if (targetRef.current) {
+        if (targetRef.current.offsetWidth == undefined) {
+          setTimeout(test_dimensions, 250);
+        }
+        else {
+          setDimensions({
+            width: targetRef.current.clientWidth,
+            height: targetRef.current.offsetHeight
+          });
+        }
+        
+      }
+    }
+
+    // This sets the dimensions on the first render
+    useLayoutEffect(() => {
+      test_dimensions();
+    }, []);
+
+    
+
+    // every time the window is resized, the timer is cleared and set again
+    // the net effect is the component will only reset after the window size
+    // is at rest for the duration set in RESET_TIMEOUT.  This prevents rapid
+    // redrawing of the component for more complex components such as charts
+    window.addEventListener('resize', ()=>{
+      clearInterval(movement_timer);
+      movement_timer = setTimeout(test_dimensions, RESET_TIMEOUT);
+    });
+
+  };
 
 interface Props {
   pageType?: string;
@@ -29,25 +77,13 @@ interface Props {
 
 function App({ pageType, locale, filters }: Props): JSX.Element {
   const { portalCode, objectCode } = useContext(AppContext);
+  const [width, setWidth] = useState(0);
 
-  let [width, setWidth] = useState(getWidth());
-
-  // in this case useEffect will execute only once because
-  // it does not have any dependencies.
-  useEffect(() => {
-    const resizeListener = () => {
-      // change width from the state object
-      setWidth(getWidth());
-    };
-    // set resize listener
-    window.addEventListener('resize', resizeListener);
-
-    // clean up function
-    return () => {
-      // remove resize listener
-      window.removeEventListener('resize', resizeListener);
-    };
+  const ref = useRef(null)
+  useLayoutEffect(() => {
+    setWidth(ref.current.getBoundingClientRect().width);
   }, []);
+  console.log(width)
 
   const { loading, error, data } = useQuery(PORTAL_QUERY, {
     variables: { id: portalCode }
@@ -122,7 +158,7 @@ function App({ pageType, locale, filters }: Props): JSX.Element {
     );
   }
 
-  return <div className={width < 875 ? 'bu-smaller' : ''}>{page}</div>;
+  return <div className={width < 875 ? 'bu-smaller' : 'bu-large'} ref={ref}>{page}</div>;
 }
 
 App.defaultProps = {
