fabff2102a5974e36236bffce97d4a70ed8893ff
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2023-08-04 20:34:11 +0200

    Add note and link to reviews

--- src/components/ReviewsPage/note.tsx
+++ src/components/ReviewsPage/note.tsx
@@ -4,7 +4,11 @@ import { FormattedMessage } from "react-intl";
 function Note() {
     return (
         <div className="bu_reviews__note">
-            <FormattedMessage id="reviews_note" />
+            {/* <FormattedMessage id="reviews_note" />
+            <br /> */}
+            <a href="https://www.bukazu.com" target="_blank" rel="noopener noreferrer">
+                <FormattedMessage id="reviews_note_link" />
+            </a>
         </div>
     )
 
