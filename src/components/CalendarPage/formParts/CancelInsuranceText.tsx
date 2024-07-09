89d54afe9ae47196f076746f8257fb7d3bc2c6f0
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-07-09 13:27:04 +0200

    refactor: Update cancel insurance text and translations

--- src/components/CalendarPage/formParts/CancelInsuranceText.tsx
+++ src/components/CalendarPage/formParts/CancelInsuranceText.tsx
@@ -21,23 +21,27 @@ const CancelInsuranceText = () => {
       </p>
 
       <h3>
-        <FM id="terms_and_costs" />
+        <FM id="cancel_insurance_important" />
       </h3>
-      <h4>
-        <FM id="costs_normal_cancel_insurance" />
-      </h4>
       <p>
-        <FM id="666_costs" />
+        <FM id="cancel_insurance_important_message" />
       </p>
-      <h4>
-        <FM id="costs_allrisk_cancel_insurance" />
-      </h4>
+
+      <h3>
+        <FM id="cancel_insurance_for_whom" />
+      </h3>
       <p>
-        <FM id="847_costs" />
+        <FM id="cancel_insurance_for_whom_explain" />
       </p>
+      <h3>
+        <FM id="cancel_insurance_questions" />
+      </h3>
       <p>
-        <FM id="more_information" />
+        <FM id="cancel_insurance_questions_explain" />
       </p>
+      <h3>
+        <FM id="terms" />
+      </h3>
       <a
         href={`https://api.bukazu.com/files/${locale}/insurance.pdf`}
         target="_blank"
@@ -45,34 +49,6 @@ const CancelInsuranceText = () => {
       >
         <FM id="show_terms" />
       </a>
-      <h3>
-        <FM id="terms" />
-      </h3>
-      <ul>
-        <li>
-          <FM id="9persons_9addresses" />
-        </li>
-        <li>
-          <strong>
-            <FM id="or" />
-          </strong>
-        </li>
-        <li>
-          <FM id="9persons_4addresses" />
-        </li>
-      </ul>
-
-      <hr />
-      <h2>
-        <FM id="poliscosts" />
-      </h2>
-      <p>
-        <FM id="poliscosts_are" />
-      </p>
-
-      <p>
-        <FM id="youwillrecieve" />
-      </p>
     </>
   );
 };
